a28d34751933bcb82aa2427e747a9ad00c8718dd
ktuenn <tuyenhkim@gmail.com>
2024-05-13 08:05:42 +0700

    update

---
+++ PostgreSQL/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="E:\download\odoo15\PostgreSQL\bin";%PATH%
+@SET PGDATA=E:\download\odoo15\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=openpg
+@SET PGPORT=5432
+@SET PGLOCALEDIR=E:\download\odoo15\PostgreSQL\share\locale
+
+                            
